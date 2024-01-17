0230211680758bdda4c80b66535a1af7d19a3603
Jean-Marc Leyman <jmleyman@gmail.com>
2024-01-17 22:22:02 +0100

    Update main updateQuality methode to take in account new factory enum management

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,29 @@
 package com.gildedrose;
 
-class GildedRose {
-    Item[] items;
+/**
+ * The GildedRose class represents the inventory system of a shop called "Gilded Rose".
+ * It manages the items in the inventory and updates their properties based on the rules of the shop.
+ */
+public class GildedRose {
 
-    public GildedRose(Item[] items) {
+    protected final Item[] items;
+
+    /**
+     * The GildedRose class represents the inventory system of a shop called "Gilded Rose".
+     * It manages the items in the inventory and updates their properties based on the rules of the shop.
+     */
+    public GildedRose(Item... items) {
         this.items = items;
     }
 
+    /**
+     * Updates the quality of all items in the inventory based on the rules of the shop.
+     * It iterates over the items array and calls the update method on each ItemWrapper object returned by the ItemFactory.getByPatternName method.
+     * The update method is responsible for updating the quality of the item, its sell-in value, and performing any additional required update.
+     */
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+        for (Item item : items) {
+            ItemFactory.getByPatternName(item).update();
         }
     }
 }
